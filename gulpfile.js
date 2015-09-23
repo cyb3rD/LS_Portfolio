e08b5ca64cc8b3c3a4064bd1f09900877d09f237
Nikolay Ignatyev <noilgt@gmail.com>
2015-09-23 13:37:37 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require("gulp"),
+	browserSync = require ("browser-sync");
+// Сервер
+gulp.task('server', function () {
+	browserSync ({
+		port: 9000,
+		server: {
+			baseDir: 'app'
+		}
+	});
+});
+// слежка
+gulp.task('watch', function () {
+	gulp.watch ([
+		'app/*.html',
+		'app/js/**/*.js',
+		'app/css/**/*.css'
+	]).on('change', browserSync.reload);
+});
+//Задача по умолчанию
+gulp.task('default', ['server', 'watch']);
