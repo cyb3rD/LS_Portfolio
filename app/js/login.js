ae996b0db65f570f3e4ac3efe650eb1e166b8614
Nikolay Ignatyev <noilgt@gmail.com>
2015-10-05 11:44:08 +0300

    Login page using bpopup plugin

---
+++ app/js/login.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	console.log('js Started! Very funny!');
+	
+	$('.login-form').bPopup({
+		opacity: 0,
+		speed: 950,
+		transition: 'slideIn',
+		transitionClose: 'slideBack'
+	});
+
+	
+});
