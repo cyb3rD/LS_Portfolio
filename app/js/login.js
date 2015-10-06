875e9cda227b4c23e6c63980d15d1f11e18aa353
Nikolay Ignatyev <noilgt@gmail.com>
2015-10-06 22:29:37 +0300

    New version of authentication page (in standalone file)

---
+++ app/js/login.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	
+	// Show Login Page
+		$('.login-form').bPopup({
+			opacity: 0.7,
+			speed: 950,
+			transition: 'slideDown',
+			transitionClose: 'slideUp'
+		});
+});
--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,5 @@
 $(document).ready(function() {
 
-	// Show Login Page
-	$('.lock').bind ('click', function (e) {
-		//e.preventDefault;
-
-		$('.login-form').bPopup({
-			positionStyle: 'fixed',
-			opacity: 0.7,
-			speed: 950,
-			transition: 'slideDown',
-			transitionClose: 'slideUp'
-		});
-
-	});
+	// Empty Stubmle
 
 });
