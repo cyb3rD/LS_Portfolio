9041ebb5634a23c11d7f0809385561e3a8a1e3d6
Nikolay Ignatyev <noilgt@gmail.com>
2015-10-06 18:06:34 +0300

    Added LoginPage popup. Using bpopup plugin.

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
-	console.log('js Started! Very funny!');
-	console.log($);
 
+	// Show Login Page
+	$('.lock').bind ('click', function (e) {
+		//e.preventDefault;
+
+		$('.login-form').bPopup({
+			positionStyle: 'fixed',
+			opacity: 0.7,
+			speed: 950,
+			transition: 'slideDown',
+			transitionClose: 'slideUp'
+		});
+
+	});
 
 });
