cec47893377638d9a4bf2f3437b3c7b14875e49c
Nikolay Ignatyev <noilgt@gmail.com>
2015-10-14 19:14:27 +0300

    Added gulp package wiredep Write "draft" task for deploy. Remove unnecessary js files

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,9 @@ var gulp = require("gulp"),
 	useref = require("gulp-useref"),
 	gulpif = require("gulp-if"),
 	uglify = require("gulp-uglify"),
-	minifyCss = require("gulp-minify-css");
-// Сервер
+	minifyCss = require("gulp-minify-css"),
+	wiredep = require("wiredep").stream;
+// Server
 gulp.task('server', function () {
 	browserSync ({
 		port: 9000,
@@ -13,7 +14,25 @@ gulp.task('server', function () {
 		}
 	});
 });
-// слежка
+
+// Attach bower files
+gulp.task('wiredep-bower', function() {
+	return gulp.src('./app/*.html')
+		.pipe(wiredep({
+			directory: './app/bower',
+			overrides: {
+				//rewrite qtip2 bower.json
+				"qTip2": {
+					"main": ["./jquery.qtip.min.js", "./jquery.qtip.css"],
+					"dependencies": {"jquery": ">=1.6.0"}
+				}
+			}
+			//
+		}))
+		.pipe(gulp.dest('./app'));
+});
+
+// Watch
 gulp.task('watch', function () {
 	gulp.watch ([
 		'app/*.html',
@@ -21,8 +40,13 @@ gulp.task('watch', function () {
 		'app/css/**/*.css'
 	]).on('change', browserSync.reload);
 });
-//Задача по умолчанию
+
+
+// Default task
+//gulp.task('default', ['server', 'wiredep-bower', 'watch']);
 gulp.task('default', ['server', 'watch']);
+
+
 
 // Move files to DIST
 gulp.task('useref', function () {
@@ -34,4 +58,13 @@ gulp.task('useref', function () {
 		.pipe(assets.restore())
 		.pipe(useref())
 		.pipe(gulp.dest('dist'));
+});
+// Dev server
+gulp.task('dev-server', function () {
+	browserSync ({
+		port: 9999,
+		server: {
+			baseDir: 'dist'
+		}
+	});
 });
